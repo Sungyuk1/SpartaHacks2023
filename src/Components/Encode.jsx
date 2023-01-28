d60554c5cabf121b6590a946659498e732bd3185
Sungyu Kwon <kwonsun7@msu.edu>
2023-01-28 16:22:39 -0500

    Encode Image Button

--- src/Components/Encode.jsx
+++ src/Components/Encode.jsx
@@ -12,14 +12,19 @@ function Encode() {
         setWalletKeys(e.target.value)
     }
 
+    function onEncodeButtonClick(){
+        alert('Encode Button Clicked')
+    }
+
   return (
     <div className='flex flex-col bg-blue-400'>
         <p className='text-4xl'>Encode Screen</p>
         <input className="m-4" type="file" accept="image/*" onChange={onImageChange}/>
         <input className="w-1/4 m-4" type="text" onChange={onTextInput}/>
         <p className='mx-4'>{walletkeys}</p>
-
-        
+        <div className="bg-black w-2/12 rounded-md flex justify-center mx-4">
+            <button className="text-white" onClick={onEncodeButtonClick}>Encode Image</button>
+        </div>
     </div>
   )
 }
